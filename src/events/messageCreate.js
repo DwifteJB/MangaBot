81633d2b606fbbfa755214d0d3668f2d20175ec8
DwifteJB <dwifte@outlook.com>
2024-07-28 18:48:05 +0100

    feat: fix

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -28,48 +28,55 @@ module.exports = async (client, message) => {
         // manga lookup
         const UserSettings = await Settings.get(message.author.id)
         const API = UserSettings.api
-        let Embeds = await Manga.GetEmbeds(API,matches[1])
+
+        try {
+            let Embeds = await Manga.GetEmbeds(API,matches[1])
         
-        if (Embeds == false) {
-            await message.reply("Couldn't find any manga with that name.");
-            return;
-        } else if (Embeds == true) {
-            await message.reply("We couldn't find the API used in your settings: "+API+"\nIt's either down for maintenance, or no longer supported.");
-            return;
+            if (Embeds == false) {
+                await message.reply("Couldn't find any manga with that name.");
+                return;
+            } else if (Embeds == true) {
+                await message.reply("We couldn't find the API used in your settings: "+API+"\nIt's either down for maintenance, or no longer supported.");
+                return;
+            }
+     
+            const row = new ActionRowBuilder()
+                .addComponents(
+                    new SelectMenuBuilder()
+                        .setCustomId('select')
+                        .setPlaceholder(Embeds[0].data.title)
+                );
+            for (index in Embeds) {
+            var title
+            if (Embeds[index].data.title) {
+                title = Embeds[index].data.title.substring(0,80)+"..."
+            } else {
+                title = "Not Found"	
+            }
+                row.components[0].addOptions({label:title ,description:Embeds[index].data.description.substring(0,80)+"...",value:index})
+            }
+            const SentMSG = await message.reply({components: [row], embeds: [Embeds[0]] });
+            const filter = i => {
+                i.deferUpdate();
+    
+                return i.user.id === message.author.id;
+            };
+            
+            const collector = SentMSG.createMessageComponentCollector({ filter, componentType: ComponentType.StringSelect, time: 60000 })
+            collector.on("collect", interaction => {
+                row.components[0].setPlaceholder(Embeds[interaction.values[0]].data.title || "Not Found!")
+                SentMSG.edit({components: [row], embeds: [Embeds[interaction.values[0]]] })
+            });
+            collector.on("end", c => {
+                SentMSG.edit({components: []})
+            })
+            collector.on("error", c => {
+                SentMSG.edit({components: []})
+            })
+        } catch (error) {
+            message.reply("An error occured while trying to fetch the manga data.")
         }
- 
-
-
-		const row = new ActionRowBuilder()
-			.addComponents(
-				new SelectMenuBuilder()
-					.setCustomId('select')
-					.setPlaceholder(Embeds[0].data.title)
-			);
-        for (index in Embeds) {
-		var title
-		if (Embeds[index].data.title) {
-			title = Embeds[index].data.title.substring(0,80)+"..."
-		} else {
-			title = "Not Found"	
-		}
-            row.components[0].addOptions({label:title ,description:Embeds[index].data.description.substring(0,80)+"...",value:index})
-        }
-		const SentMSG = await message.reply({components: [row], embeds: [Embeds[0]] });
-        const filter = i => {
-            i.deferUpdate();
-
-            return i.user.id === message.author.id;
-        };
-        
-        const collector = SentMSG.createMessageComponentCollector({ filter, componentType: ComponentType.StringSelect, time: 60000 })
-        collector.on("collect", interaction => {
-            row.components[0].setPlaceholder(Embeds[interaction.values[0]].data.title || "Not Found!")
-            SentMSG.edit({components: [row], embeds: [Embeds[interaction.values[0]]] })
-        });
-        collector.on("end", c => {
-            SentMSG.edit({components: []})
-        })
+      
         
     }
 
